cc0e38d3658ecf64457cf771a3ba91159a91b7cc
sebastien <se.rocca@laposte.net>
2022-05-03 17:49:01 +0200

    TP4 partie 3 - Amélioration de l'implémentation

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -7,13 +7,17 @@ import java.util.Map;
 public class PokemonFactory implements IPokemonFactory{
 
 
+    private Pokedex pokedex;
+
+    public void setPokedex(Pokedex pokedex)
+    {
+        this.pokedex = pokedex;
+    }
 
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) throws PokedexException {
 
-        PokemonMetadataProvider metaProvider = new PokemonMetadataProvider();
-
-        PokemonMetadata metadata = metaProvider.getPokemonMetadata(index);
+        PokemonMetadata metadata = pokedex.getPokemonMetadata(index);
 
         return new Pokemon(index, metadata.getName(), metadata.getAttack(), metadata.getDefense(), metadata.getStamina(), cp, hp, dust, candy, 0);
     }
